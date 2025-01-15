08ce6c6968ee8e71cea28ad2e0e40077738d4425
oscar <aaronjones2429@gmail.com>
2025-01-15 14:13:58 -0500

    subiendo final pagina inicio

--- interactive.js
+++ interactive.js
@@ -1,5 +1,14 @@
-const boton = document.querySelector('#boton4')
+function iniciarMap(){
+    var coord = {lat:-44.5956145 ,lng: -78.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
 
-boton.addEventListener('click', ()=> {
-    alert("EMMANUEL ES GAY")
-})
+
+// Api para Google Maps de la página principal del proyecto
