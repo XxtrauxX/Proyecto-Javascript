66883f9a9fe7266f0b9f2ebc4d04f57894c9b550
oscar <aaronjones2429@gmail.com>
2025-01-16 03:31:03 -0500

    subiendo cambios

--- interactive.js
+++ interactive.js
@@ -1,14 +1,43 @@
-function iniciarMap(){
-    var coord = {lat:-44.5956145 ,lng: -78.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
+
+// sección de botones
+
+const BotonHero = document.querySelector('#botonhero')
+const boton4 = document.querySelector('#boton4')
+const boton5 = document.querySelector('.boton5')
+const correoBoton = document.querySelector('#correo')
 
 
-// Api para Google Maps de la página principal del proyecto
+
+
+BotonHero.addEventListener('click', ()=> {
+
+  window.location.href="/Proyecto-Javascript/reservas.html"
+  
+})
+
+
+boton4.addEventListener('click', ()=> {
+
+  window.location.href="/Proyecto-Javascript/reservas.html"
+  
+})
+
+
+boton5.addEventListener('click', ()=> {
+
+  window.location.href="/Proyecto-Javascript/reservas.html"
+  
+})
+
+
+
+correoBoton.addEventListener('click', (e)=> {
+   e.preventDefault()
+  alert("Gracias por su registro!!")
+  window.location.href="https://workspace.google.com/intl/es-419/gmail/"
+  
+})
+
+
+
+
---
+++ reservas.js
@@ -0,0 +1,48 @@
+const prueba = document.querySelector('#prueba')
+const contendor = document.querySelector('#contenidojs')
+const boton = document.querySelector('#boton67')
+
+
+prueba.addEventListener('click', ()=> {
+    const nuevodiv = document.createElement('div')
+    nuevodiv.classList.add('nuevodiv')
+    fetch(" http://localhost:3000/habitaciones/1")
+    .then( res => res.json())
+    .then((datos)=> {
+        console.log(datos)
+
+
+        nuevodiv.innerHTML = `
+        <div class="grid md:grid-cols-2 gap-6">
+                
+        <div class="relative h-64 md:h-full">
+          <img 
+            src="/Proyecto-Javascript/img/Habitaciones.jpeg"
+            class="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+  
+       
+        <div class="p-6 md:p-8 flex flex-col justify-center">
+          <h2 class="text-3xl font-bold mb-4">${datos.nombre}</h2>
+          <h3 class"text-3xl font-bold mb-4">Precio:$ ${datos.precio}</3>
+          <p class="text-gray-600 mb-6">
+            ${datos.descripcion}
+          </p>
+          
+    
+            <button class="inline-flex justify-center items-center px-6 py-3 rounded-lg bg-blue-900 text-white font-medium hover:bg-blue-800 transition-colors w-full sm:w-auto text-center"> VER DETALLES</button>
+        </div>
+      </div>
+    </div>        
+        `;
+
+        
+        contendor.append(nuevodiv)
+
+
+    })
+})
+
+
+
